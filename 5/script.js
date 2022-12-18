2acb1290c876360aaec67dddf9b253adc0451d8b
Pavel_Zhandin <pavelzhandin@gmail.com>
2022-12-18 20:24:09 +0300

    task 5 done

--- 5/script.js
+++ 5/script.js
@@ -1,25 +1,26 @@
 let i = 0;
+let timeStart;
 
 function infiniteRender(i) {
-  console.log(i);
-  if (i < 100) {
-    const containers = document.querySelectorAll(`.innerContainer-${i}`);
-    const innerContainer = document.createElement("div");
+  // const time = Date.now();
+  console.log(i, Date.now() - timeStart);
 
-    // innerContainer.classList.add([`innerContainer-${i}`, "container"]);
+  const containers = document.querySelectorAll(`.innerContainer-${i}`);
+  const innerContainer = document.createElement("div");
 
-    containers.forEach((container) => {
-      container.innerHTML = `<div class="container innerContainer-${
-        i + 1
-      }"></div><div class="container innerContainer-${i + 1}"></div>`;
-    });
-
-    infiniteRender(i + 1);
-  } else return;
+  containers.forEach((container) => {
+    container.innerHTML = `<div class="container innerContainer-${
+      i + 1
+    }"></div><div class="container innerContainer-${i + 1}"></div>`;
+  });
+  setTimeout(infiniteRender(i + 1), 50);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  timeStart = Date.now();
+  console.log(timeStart);
   infiniteRender(0);
 });
 
-// display flex - 23
+// display flex - 23 depth
+// float: left - 23 depth
